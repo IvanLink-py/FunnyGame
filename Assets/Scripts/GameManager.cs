988fd8b3a7066bad780516f99ff518b57c887b07
IvanLink-py <ostaninivan8@gmail.com>
2023-01-05 22:54:11 +0500

    База GameManager'а

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+
+    private void Awake() => _instance = this;
+    
+    
+    
+}

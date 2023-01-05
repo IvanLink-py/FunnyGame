8b651d8d85120d2e96131c2e3ec330e7a3460ed6
IvanLink-py <ostaninivan8@gmail.com>
2023-01-05 20:34:02 +0500

    Базовое управление игроком

--- Assets/GameObjects/Player/PlayerControl.cs
+++ Assets/GameObjects/Player/PlayerControl.cs
@@ -1,18 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Debug = System.Diagnostics.Debug;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float speed = 5;
+    private Rigidbody2D _rigidbody2D;
+    private Camera _mainCamera;
+
+    private void Start()
     {
-        
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _mainCamera = Camera.main;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+        Aim();
+    }
+
+    private void Aim()
+    {
+        var diff = _mainCamera.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        diff.Normalize();
+
+        var rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z);
+    }
+
+    private void Movement()
+    {
+        var control = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        _rigidbody2D.velocity = control * speed;
     }
 }

3d20700bdd5b36934072292318579ac9859b41d8
IvanLink-py <ostaninivan8@gmail.com>
2023-01-08 00:09:23 +0500

    Следование камеры за курсором

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -7,14 +7,24 @@ public class CameraControl : MonoBehaviour
 {
     [SerializeField] private Transform player;
     [SerializeField] private float speedMod = 100;
+    public float fov;
 
     private void FixedUpdate()
     {
         var playerPos = player.position;
+        var screenCoords = Camera.main.ScreenToViewportPoint(Input.mousePosition) - Vector3.one / 2f;
+        var mouseOffset = new Vector3(Screen.width * screenCoords.x, Screen.height * screenCoords.y) * (fov / 100);
+        CameraMove(playerPos + mouseOffset);
+    }
+
+    private void CameraMove(Vector3 playerPos)
+    {
         var distance = (transform.position - playerPos).magnitude;
-        var speed = 1 / -(speedMod * distance) + 1;                 // Расчёт скорости перемешения камеры
-                                                                        // на основании дистанции до игрока
-                                                                        // Дальше -> Быстрее
+
+        var speed = 1 / -(speedMod * distance) + 1;
+        // Расчёт скорости перемешения камеры
+        // на основании дистанции до игрока
+        // Дальше -> Быстрее
 
         transform.position = Vector3.Lerp(transform.position, playerPos, speed) - Vector3.forward;
     }

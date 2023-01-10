b9f5b6519fde8dee252dd650e5214403e14cb55c
IvanLink-py <ostaninivan8@gmail.com>
2023-01-10 20:29:41 +0500

    Реализация поиска предмета в базе по его id

--- Assets/GameObjects/Items/ItemDB.cs
+++ Assets/GameObjects/Items/ItemDB.cs
@@ -1,7 +1,26 @@
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "ItemDB", menuName = "Item/Item db", order = 0)]
 public class ItemDb : ScriptableObject
 {
+    public List<ItemInfo> infoList = new ();
 
+    [CanBeNull]
+    public ItemInfo GetItemInfo(string id)
+    {
+        var l = infoList.Where(i => i.id.Equals(id)).ToList();
+        switch (l.Count)
+        {
+            case > 1:
+                Debug.Log($"В базе более 1 предмета с id \"{id}\"");
+                break;
+            case 0:
+                return null;
+        }
+
+        return l[0];
+    }
 }
